c0a7d817aa53c225bec503cb61f6be6f43860e6d
ansh-agrawal <141269969+ansh-agrawal@users.noreply.github.com>
2024-08-25 12:02:10 +0530

    Frontend and backend

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app
+  .route("/bfhl")
+  .get((req, res) => {
+    res.status(200).json({ operation_code: 1 });
+  })
+  .post((req, res) => {
+    const data = req.body.data || [];
+    const numbers = [];
+    const alphabets = [];
+    let highest_alphabet = "";
+
+    for (const item of data) {
+      if (!isNaN(item)) {
+        numbers.push(item);
+      } else if (item.length === 1 && isNaN(item)) {
+        alphabets.push(item);
+        if (
+          !highest_alphabet ||
+          item > highest_alphabet
+        ) {
+          highest_alphabet = item;
+        }
+      }
+    }
+
+    res.json({
+      is_success: true,
+      user_id: "ansh_agrawal_06092002",
+      email: "ansh.agrawal2021a@vitstudent.ac.in",
+      roll_number: "21BCE5596",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
+    });
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
